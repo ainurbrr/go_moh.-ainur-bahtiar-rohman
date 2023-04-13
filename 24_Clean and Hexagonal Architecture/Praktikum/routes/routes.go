0eee05e5899721729f3425f5b3aa85eb8219be7e
ainurbrr <ainurbahtiarr@gmail.com>
2023-04-13 21:01:16 +0700

    update push

---
+++ 24_Clean and Hexagonal Architecture/Praktikum/routes/routes.go
@@ -0,0 +1,44 @@
+package routes
+
+import (
+	"praktikum/constants"
+	"praktikum/controllers"
+	"praktikum/middlewares"
+
+	"github.com/labstack/echo/v4"
+	mid "github.com/labstack/echo/v4/middleware"
+	m "github.com/labstack/echo-jwt"
+
+)
+
+func New() *echo.Echo {
+	e := echo.New()
+	// Route / to handler function
+	middlewares.LogMiddleware(e)
+	e.Pre(mid.RemoveTrailingSlash())
+
+	eUsersJwt := e.Group("/users")
+	eUsersJwt.GET("", controllers.GetUsersController, m.JWT([]byte(constants.SECRET_JWT)))
+	eUsersJwt.GET("/:id", controllers.GetUserController, m.JWT([]byte(constants.SECRET_JWT)))
+	eUsersJwt.POST("", controllers.CreateUserController)
+	eUsersJwt.POST("/login", controllers.LoginUserController)
+	eUsersJwt.DELETE("/:id", controllers.DeleteUserController, m.JWT([]byte(constants.SECRET_JWT)))
+	eUsersJwt.PUT("/:id", controllers.UpdateUserController, m.JWT([]byte(constants.SECRET_JWT)))
+
+	eBooksJwt := e.Group("/books")
+	eBooksJwt.Use(m.JWT([]byte(constants.SECRET_JWT)))
+	eBooksJwt.GET("", controllers.GetBooksController)
+	eBooksJwt.GET("/:id", controllers.GetBookController)
+	eBooksJwt.POST("", controllers.CreateBookController)
+	eBooksJwt.DELETE("/:id", controllers.DeleteBookController)
+	eBooksJwt.PUT("/:id", controllers.UpdateBookController)
+
+	eBlogsJwt := e.Group("/blogs")
+	eBlogsJwt.GET("", controllers.GetBlogsController)
+	eBlogsJwt.GET("/:id", controllers.GetBlogController)
+	eBlogsJwt.POST("", controllers.CreateBlogController)
+	eBlogsJwt.DELETE("/:id", controllers.DeleteBlogController)
+	eBlogsJwt.PUT("/:id", controllers.UpdateBlogController)
+
+	return e
+}
